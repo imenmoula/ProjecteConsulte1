7605db5362ac67229fb9c388c093ec6cff0f72b1
moulaimen <votre_email>
2024-12-10 21:01:58 +0100

    fix : fix blade show

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,15 @@
 
+@extends('dashboard')
 
+@section('content')
+<div class="container">
+    <h1>Modifier mon Profil</h1>
+
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 <h1>Welcome, {{ $user->name }}</h1>
 <p>Email: {{ $user->email }}</p>
 <p>Job: {{ $user->job }}</p>
@@ -7,3 +17,6 @@
 <p>Experience: {{ $user->nb_experience }} years</p>
 <img src="{{ Storage::url($user->image) }}" alt="Profile Image">
 <a href="{{ route('profile.edit') }}">Edit Profile</a>
+
+</div>
+@endsection
